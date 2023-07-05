c21c65694bdd78bfc8dc0becfb5f514133557587
Nick Sutterer <apotonick@gmail.com>
2023-07-05 16:13:09 +0200

    fix test_helper.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,8 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "trailblazer/workflow"
 
 require "minitest/autorun"
+require "pp"
+require "trailblazer/activity/testing"
 
 class Minitest::Spec
   def assert_equal(expected, asserted, *args)
