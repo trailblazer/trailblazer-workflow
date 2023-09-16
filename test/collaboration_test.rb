4e079eb000131c3485cf3dfe00b00d129bc83f65
Nick Sutterer <apotonick@gmail.com>
2023-09-16 16:45:44 +0200

    add ui lane.

--- test/collaboration_test.rb
+++ test/collaboration_test.rb
@@ -2,14 +2,13 @@ require "test_helper"
 
 class CollaborationTest < Minitest::Spec
   it "what" do
-    implementing = Trailblazer::Activity::Testing.def_steps(:create, :update, :notify_approver, :reject, :approve, :revise, :publish, :archive, :delete)
-
     moderation_json = File.read("test/fixtures/v1/moderation.json")
-
     signal, (ctx, _) = Trailblazer::Workflow::Generate.invoke([{json_document: moderation_json}, {}])
 
     article_moderation_intermediate = ctx[:intermediates]["article moderation"]
     # pp article_moderation_intermediate
+
+    implementing = Trailblazer::Activity::Testing.def_steps(:create, :update, :notify_approver, :reject, :approve, :revise, :publish, :archive, :delete)
 
     lane_activity = Trailblazer::Workflow::Collaboration.Lane(
       article_moderation_intermediate,
@@ -23,6 +22,35 @@ class CollaborationTest < Minitest::Spec
       "Publish" => implementing.method(:publish),
       "Archive" => implementing.method(:archive),
       "Delete" => implementing.method(:delete),
+    )
+
+
+    article_moderation_intermediate = ctx[:intermediates]["<ui> author workflow"]
+    # pp article_moderation_intermediate
+
+    implementing = Trailblazer::Activity::Testing.def_steps(:create_form, :create, :update_form, :update, :notify_approver, :reject, :approve, :revise, :publish, :archive, :delete, :delete_form, :cancel, :revise_form,
+      :create_form_with_errors, :update_form_with_errors, :revise_form_with_errors)
+
+    lane_activity_ui = Trailblazer::Workflow::Collaboration.Lane(
+      article_moderation_intermediate,
+
+      "Create form" => implementing.method(:create_form),
+      "Create" => implementing.method(:create),
+      "Update form" => implementing.method(:update_form),
+      "Notify approver" => implementing.method(:notify_approver),
+      "Publish" => implementing.method(:publish),
+      "Delete" => implementing.method(:delete),
+      "Delete? form" => implementing.method(:delete_form),
+      "Cancel" => implementing.method(:cancel),
+      "Revise" => implementing.method(:revise),
+      "Revise form" => implementing.method(:revise_form),
+      "Update" => implementing.method(:update),
+      "Create form with errors" => implementing.method(:create_form_with_errors),
+      "Update form with errors" => implementing.method(:update_form_with_errors),
+      "Revise form with errors" => implementing.method(:revise_form_with_errors),
+      "Archive" => implementing.method(:archive),
+      # "Approve" => implementing.method(:approve),
+      # "Reject" => implementing.method(:reject),
     )
 
 
