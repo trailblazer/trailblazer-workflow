a4b6f07652bd819338ffce8e1995e57f82201baa
Nick Sutterer <apotonick@gmail.com>
2023-09-16 16:33:43 +0200

    start working on collaboration_test.

---
+++ test/collaboration_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+class CollaborationTest < Minitest::Spec
+  it "what" do
+    implementing = Trailblazer::Activity::Testing.def_steps(:create, :update, :notify_approver, :reject, :approve, :revise, :publish, :archive, :delete)
+
+    moderation_json = File.read("test/fixtures/v1/moderation.json")
+
+    signal, (ctx, _) = Trailblazer::Workflow::Generate.invoke([{json_document: moderation_json}, {}])
+
+    article_moderation_intermediate = ctx[:intermediates]["article moderation"]
+    # pp article_moderation_intermediate
+
+    lane_activity = Trailblazer::Workflow::Collaboration.Lane(
+      article_moderation_intermediate,
+
+      "Create" => implementing.method(:create),
+      "Update" => implementing.method(:update),
+      "Approve" => implementing.method(:approve),
+      "Notify approver" => implementing.method(:notify_approver),
+      "Revise" => implementing.method(:revise),
+      "Reject" => implementing.method(:reject),
+      "Publish" => implementing.method(:publish),
+      "Archive" => implementing.method(:archive),
+      "Delete" => implementing.method(:delete),
+    )
+
+
+  end
+end
