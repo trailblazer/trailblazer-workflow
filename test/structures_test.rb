fab714b9186f59f0a2647a3b0d87b9e5ba592d9c
Nick Sutterer <apotonick@gmail.com>
2024-02-16 15:32:31 +0100

    start structures_test.rb.

---
+++ test/structures_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class StructuresTest < Minitest::Spec
+  it "Collaboration::Positions" do
+    activity_a = "A"
+    suspend_a  = "suspend:a"
+    activity_b = "B"
+    suspend_b = "suspend:b"
+
+    #@ Position.new
+    position_a = Trailblazer::Workflow::Collaboration::Position.new(activity_a, suspend_a)
+    position_b = Trailblazer::Workflow::Collaboration::Position.new(activity_b, suspend_b)
+
+    #@ Position#to_a
+    assert_equal position_a.to_a.inspect, %(["A", "suspend:a"])
+
+    #@ Positions.new
+    positions = Trailblazer::Workflow::Collaboration::Positions.new([position_a, position_b])
+
+    #@ Positions.collect automatically decomposes each iterated position.
+    assert_equal positions.collect { |activity, task| [activity, task] }.inspect, %([[\"A\", \"suspend:a\"], [\"B\", \"suspend:b\"]])
+  end
+end
