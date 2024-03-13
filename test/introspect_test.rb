351695e66967fbafc1f0a86fcbfc38dd234f361d
Nick Sutterer <apotonick@gmail.com>
2024-03-13 07:57:56 +0100

    test Introspect::StateTable.

--- test/introspect_test.rb
+++ test/introspect_test.rb
@@ -1,18 +1,24 @@
 require "test_helper"
 
-class StateTableTest < Minitest::Spec
-  include BuildSchema
-  include DiscoveredStates
+class IntrospectStateTableTest < Minitest::Spec
+  it "StateTable" do
+    iteration_set, lanes_cfg = fixtures()
 
-  it "what" do
-    # TODO: this is something that shouldn't be done every time.
-    states, lanes_sorted, lanes_cfg, schema, message_flow = states()
-    iteration_set = Trailblazer::Workflow::Introspect::Iteration::Set.from_discovered_states(states, lanes_cfg: lanes_cfg)
-
-    puts output = Trailblazer::Workflow::Introspect::StateTable::Generate.(iteration_set, lanes_cfg: lanes_cfg, namespace: "App::Posting")
-
-    # TODO: we could store the {:id} field in a serialized doc, and grab positions from iteration_set when deserializing.
-
-    assert_equal output, File.read("test/expected/posting.state_guards.rb")
+    cli_state_table = Trailblazer::Workflow::Introspect::StateTable.(iteration_set, lanes_cfg: lanes_cfg)
+    # puts cli_state_table
+    assert_equal cli_state_table,
+%(+---------------------------------+----------------------------------------+
+| state name                      | triggerable events                     |
++---------------------------------+----------------------------------------+
+| "⏸︎ Create form"                 | "☝ ⏵︎Create form"                       |
+| "⏸︎ Create"                      | "☝ ⏵︎Create"                            |
+| "⏸︎ Update form♦Notify approver" | "☝ ⏵︎Update form", "☝ ⏵︎Notify approver" |
+| "⏸︎ Update"                      | "☝ ⏵︎Update"                            |
+| "⏸︎ Delete? form♦Publish"        | "☝ ⏵︎Delete? form", "☝ ⏵︎Publish"        |
+| "⏸︎ Revise form"                 | "☝ ⏵︎Revise form"                       |
+| "⏸︎ Delete♦Cancel"               | "☝ ⏵︎Delete", "☝ ⏵︎Cancel"               |
+| "⏸︎ Archive"                     | "☝ ⏵︎Archive"                           |
+| "⏸︎ Revise"                      | "☝ ⏵︎Revise"                            |
++---------------------------------+----------------------------------------+)
   end
 end
